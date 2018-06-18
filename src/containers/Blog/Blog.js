8a081fe4c4b790f028aa3a3a7257064ad6080dda
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-18 15:48:53 +0200

    adds routing to posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -16,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render={() => <Posts />} />
+                <Route path="/new-post" exact render={() => <NewPost />} />
             </div>
         );
     }
