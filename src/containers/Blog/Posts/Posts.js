33759d2960ae81122abac7834a3aea5f559d7a31
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-22 09:26:20 +0200

    adding navigating prog

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -30,7 +29,8 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        this.props.history.push({pathname: '/' + id});
+        // this.props.history.push({'/' + id});
     }
 
     render() {
@@ -38,14 +38,16 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link 
-                        to={'/' + post.id}
-                        key={post.id}>
-                        <Post
-                            author={post.author}
-                            title={post.title}
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>);
+                    // <Link 
+                    //     to={'/' + post.id}
+                    //     key={post.id}>
+                    <Post
+                        key={post.id}
+                        author={post.author}
+                        title={post.title}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link>
+                );
             });
         }
 
