0a5cc8e14491d3309986a07fa31a7783745880f9
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-25 15:05:18 +0200

    adding guards to routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -31,7 +35,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <Posts />} />
                 <Route path="/new-post" exact render={() => <NewPost />} /> */}
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     {/* <Route path="/" component={Posts} /> */}
