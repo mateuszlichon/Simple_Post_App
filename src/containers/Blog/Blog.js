459c5c84229e04a4a6e7deaa74e2df2acb2a9d7e
mateuszlichon <mateusz.lichon@gmail.com>
2018-06-25 13:52:07 +0200

    adds dynamic url

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -15,14 +14,14 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink
-                                to="/"
+                                to="/posts"
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
                                     color: '#fa923f',
                                     textDecoration: 'underline'
                                 }}
-                            >Home</NavLink></li>
+                            >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post'
                             }}>New Post</NavLink></li>
@@ -31,10 +30,9 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <Posts />} />
                 <Route path="/new-post" exact render={() => <NewPost />} /> */}
-                <Route path="/" exact component={Posts} />
                 <Switch>
                     <Route path="/new-post" exact component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
 
             </div>
